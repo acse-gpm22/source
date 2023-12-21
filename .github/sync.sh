803a853ec2e9102d905c5cd190379bcbefd32019
acse-6cc75b90 <gpm22@ic.ac.uk>
2023-12-21 16:57:47 +0000

    Fix bugs

--- .github/sync.sh
+++ .github/sync.sh
@@ -20,12 +20,12 @@ SYNC_FILE=".github/sync.yml"
 PATTERN='.*..-repo-destination'
 
 # Write to $SYNC_FILES the repos to be synced
-REPOS=($(gh repo list --source
-         --json name
-        --jq '.[].name'
+REPOS=($(gh repo list --source \
+        --json name \
+        --jq '.[].name' \
         --limit 1000 |
         grep -e $PATTERN))
-echo "group:\n  repos: |" > ${SYNC_FILE}
+echo -e "group:\n  repos: |" > ${SYNC_FILE}
 for repo in "${REPOS[@]}"; do
     echo "    ${repo}" >> ${SYNC_FILE}
 done
@@ -34,27 +34,31 @@ done
 #   1. Current limitation to deleted files not being synced without deleteOrphaned.
 #      Since the children is populated by another repo, this flag is not a viable option
 MODIFIED_FILES=($(gh pr diff $number --name-only))
-echo "Modified Files: \n    ${MODIFIED_FILES[@]}\n"
+# echo -e "Modified Files: \n    ${MODIFIED_FILES[@]}\n"
 
 # Get list of files that won't be synced
 declare -a EXCLUDED_FILES=(README.md)
 declare -a EXCLUDED_FOLDERS=(excluded)
-for folder in "EXCLUDED_FOLDERS[@]"; do
+for folder in "${EXCLUDED_FOLDERS[@]}"; do
     FILES=($(find $folder -type f -follow -print))
     EXCLUDED_FILES=("${EXCLUDED_FILES[@]}" "${FILES[@]}")
 done
-echo "Excluded Files: \n    ${EXCLUDED_FILES[@]}\n"
+# echo -e "Excluded Files: \n    ${EXCLUDED_FILES[@]}\n"
 
 # Filter out from MODIFIED_FILES the invalid files
 for file in "${EXCLUDED_FILES[@]}"; do
-    MODIFIED_FILES=(${MODIFIED_FILES[@]//*file*/})
+    for i in "${!MODIFIED_FILES[@]}"; do
+        if [[ ${MODIFIED_FILES[i]} = $file ]]; then
+            unset "MODIFIED_FILES[i]"
+        fi
+    done
 done
-echo "Modified Files: \n    ${MODIFIED_FILES[@]}\n"
+# echo -e "Modified Files: \n    ${MODIFIED_FILES[@]}\n"
 
 # Define, for each repo, the list of files to be synced
 echo "  files:" >> ${SYNC_FILE}
 for file in "${MODIFIED_FILES[@]}"; do
-    echo "    - source: ${file}\n      dest: ${file}" >> ${SYNC_FILE}
+    echo -e "    - source: ${file}\n      dest: ${file}" >> ${SYNC_FILE}
 done
 
 # Restore the Internal Field Separator
